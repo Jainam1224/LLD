40c5b43ab97468ad3e150e3800ecd347d9f57da6
Jainam Sanghvi <jainam.sanghvi@flipkart.com>
2025-06-05 00:33:45 +0530

    accordion view

--- lowleveldesign_examples/src/App.js
+++ lowleveldesign_examples/src/App.js
@@ -5,6 +5,7 @@ import Team from "./components/Team";
 import Login from "./components/Login";
 import ProtectedRoute from "./components/ProtectedRoute";
 import { useState } from "react";
+import Accordion from "./components/Accordion/Accordion";
 
 function App() {
   const [lang, setLang] = useState("en");
@@ -13,8 +14,10 @@ function App() {
       <header className="text-2xl font-bold py-5 bg-black text-white text-center flex">
         LLD Learning
         <nav className="px-20 m-2 w-[1200px] flex justify-between text-lg">
+          <a href="/">Home </a>
           <a href="/about">About </a>
           <a href="/team">Team </a>
+          <a href="/accordion">Accordion </a>
           <a href="/login">Login </a>
         </nav>
         <select value={lang} onChange={(e) => setLang(e.target.value)}>
@@ -31,6 +34,7 @@ function App() {
             <Route path="/about" element={<About lang={lang} />} />
           </Route>
           <Route path="/team" element={<Team />} />
+          <Route path="/accordion" element={<Accordion />} />
           <Route path="/login" element={<Login />} />
         </Routes>
       </BrowserRouter>
